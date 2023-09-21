d9e6926574e8fc8c01d7155e8aa8844366c62a22
arman <arman458792@gmail.com>
2023-09-22 00:31:39 +0600

    Deploy to heroku7

--- src/main/java/kz/armank/library/dao/imp/CategoryDtoDaoImp.java
+++ src/main/java/kz/armank/library/dao/imp/CategoryDtoDaoImp.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +18,8 @@ public class CategoryDtoDaoImp implements CategoryDtoDao {
 
     @Override
     public Optional<List<CategoryDto>> getCategoryDto() {
+        List<CategoryDto> list = new ArrayList<>();
+        list.add((new CategoryDto("Роман", 100L)));
 //        return Optional.of(entityManager.createQuery(
 //                """
 //                        SELECT new kz.armank.library.dto.CategoryDto(c.name, count (b))
@@ -26,6 +29,6 @@ public class CategoryDtoDaoImp implements CategoryDtoDao {
 //                        """,
 //                CategoryDto.class)
 //                .getResultList());
-        return Optional.of(List.of(new CategoryDto("Роман", 100L)));
+        return Optional.of(list);
     }
 }
